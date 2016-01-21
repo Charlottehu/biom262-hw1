990f508ab04040f5655598008369a98a4c1f2767
Soohwan Oh <soo001@ucsd.edu>
2016-01-20 22:39:17 -0800

    conflict ??

--- tf_binding.sh
+++ tf_binding.sh
@@ -30,3 +30,4 @@ module load biotools
 touch gencode.v19.annotation.chr22.transcript.promoter.nfkb.fasta
 bedtools getfasta -s -fi GRCh37.p13.chr22.fa -bed gencode.v19.annotation.chr22.transcript.promoter.nfkb.gtf -fo gencode.v19.annotation.chr22.transcript.promoter.nfkb.fasta
 
+#"Hello I am a message in standard error (stderr)"
